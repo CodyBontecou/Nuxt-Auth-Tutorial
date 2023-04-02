f7bd327bac077f8fad2ce5d7e06137634719c0bf
Cody Bontecou <bontecouc@gmail.com>
2023-04-02 10:05:14 -1000

    signIn and signOut

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+const { status, signIn, signOut } = useSession()
+
+const loggedIn = computed(() => status.value === 'authenticated')
+
+async function handleSignIn() {
+  await signIn()
+}
+
+async function handleSignOut() {
+  await signOut()
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <button v-if="loggedIn" @click="handleSignOut">Sign Out</button>
+    <button v-else @click="handleSignIn">Sign In</button>
   </div>
 </template>

774a9955a19585231adb55babcdd9c20b4a7c0df
Cody Bontecou <bontecouc@gmail.com>
2023-04-02 10:04:08 -1000

    managing env variables using runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@sidebase/nuxt-auth'],
+  runtimeConfig: {
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    public: {
+      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    },
+  },
 })
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,12 +2,14 @@
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
 
+const runtimeConfig = useRuntimeConfig()
+
 export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
-      clientId: 'enter-your-client-id-here',
-      clientSecret: 'enter-your-client-secret-here',
+      clientId: runtimeConfig.public.GITHUB_CLIENT_ID,
+      clientSecret: runtimeConfig.GITHUB_CLIENT_SECRET,
     }),
   ],
 })

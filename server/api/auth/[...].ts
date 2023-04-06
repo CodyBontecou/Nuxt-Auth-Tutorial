728ee26800dcb8796224ba215350d8c28c52ac0f
Cody Bontecou <bontecouc@gmail.com>
2023-04-05 21:02:58 -1000

    uses /login as default for sign-in page

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,6 +5,10 @@ import GithubProvider from 'next-auth/providers/github'
 const runtimeConfig = useRuntimeConfig()
 
 export default NuxtAuthHandler({
+  pages: {
+    // Change the default behavior to use `/login` as the path for the sign-in page
+    signIn: '/login',
+  },
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({

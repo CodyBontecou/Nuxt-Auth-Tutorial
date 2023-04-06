fcc267de7b351449fcbef6d1802ca7ac8d942b6a
Cody Bontecou <bontecouc@gmail.com>
2023-04-05 21:01:39 -1000

    Reusable GithubLogin component

---
+++ components/GithubLogin.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { signIn } = useAuth()
+</script>
+
+<template>
+  <button
+    class="flex items-center border border-gray-500 px-4 py-2 rounded-lg text-gray-500 text-sm font-medium hover:text-black hover:bg-gray-100"
+    @click="signIn('github')"
+  >
+    <GithubIcon class="mr-4 h-6 w-6" />
+    Log in with Github
+  </button>
+</template>

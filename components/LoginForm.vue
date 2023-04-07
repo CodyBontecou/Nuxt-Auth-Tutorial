f37539f3052f4950fe7ee62eb86c49b18a37af9f
Cody Bontecou <bontecouc@gmail.com>
2023-04-06 18:34:25 -1000

    CredentialsProvider - email + password

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+const { signIn } = useAuth()
+
+const email = ref('')
+const password = ref('')
+</script>
+
 <template>
-  <form class="space-y-4 md:space-y-6" action="#">
+  <form
+    class="space-y-4 md:space-y-6"
+    @submit.prevent="
+      signIn('credentials', { email: email, password: password })
+    "
+  >
     <div>
       <label
         for="email"
@@ -7,12 +19,13 @@
         >Email</label
       >
       <input
+        v-model="email"
         type="email"
         name="email"
         id="email"
         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="name@company.com"
-        required=""
+        required
       />
     </div>
     <div>
@@ -22,12 +35,13 @@
         >Password</label
       >
       <input
+        v-model="password"
         type="password"
         name="password"
         id="password"
         placeholder="••••••••"
         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        required=""
+        required
       />
     </div>
     <div class="flex items-center justify-between">
@@ -38,7 +52,6 @@
             aria-describedby="remember"
             type="checkbox"
             class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-            required=""
           />
         </div>
         <div class="ml-3 text-sm">
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,6 +1,7 @@
 // file: ~/server/api/auth/[...].ts
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 const runtimeConfig = useRuntimeConfig()
 
@@ -14,6 +15,39 @@ export default NuxtAuthHandler({
     GithubProvider.default({
       clientId: runtimeConfig.public.GITHUB_CLIENT_ID,
       clientSecret: runtimeConfig.GITHUB_CLIENT_SECRET,
+    }),
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    CredentialsProvider.default({
+      name: 'Credentials',
+      authorize(credentials: any) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // NOTE: THE BELOW LOGIC IS NOT SAFE OR PROPER FOR AUTHENTICATION!
+
+        const user = {
+          email: 'test@email.com',
+          password: 'pass',
+        }
+
+        if (
+          credentials?.email === user.email &&
+          credentials?.password === user.password
+        ) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // eslint-disable-next-line no-console
+          console.error(
+            'Warning: Malicious login attempt registered, bad credentials provided'
+          )
+
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
 })

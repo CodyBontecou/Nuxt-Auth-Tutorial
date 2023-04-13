b50fd4aff7e45a808cf36f1b6ba0d0df9870585c
Cody Bontecou <bontecouc@gmail.com>
2023-04-13 09:34:03 -1000

    prisma setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
+    API_ROUTE_SECRET: process.env.API_ROUTE_SECRET,
     public: {
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
       TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
